abeb8aa9b19e60b1b90ad8b60408c193ca93b52f
Sajan <sajanmhatre29@gmail.com>
2021-09-11 01:58:00 +0530

    Mobile Number Validation

--- src/UserValidationUsingRegex.java
+++ src/UserValidationUsingRegex.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 public class UserValidationUsingRegex {
     //Instance Variable Declaration
-    private static String lastName;
-    //Method to check Last Name Validation
-    public static void lastNameValidation(){
+    private static String mobileNum;
+    //Method to check Mobile Number Validation
+    public static void mobileNumValidation(){
         //Scanner object to take Input from the User
         Scanner nw =new Scanner(System.in);
-        lastName = nw.next();
+        mobileNum = nw.next();
 //      boolean valid = name.matches("(?i)(^[a-z]+)[a-z .,-]((?! .,-)$){1,25}$");
         //Pattern to check the Last Name
-        boolean valid = lastName.matches("[A-Z]{1}[a-z]{0,15}");
+        boolean valid = mobileNum.matches("(0|91)?[7-9][0-9]{9}");
         //Condition to check Validity
         if (valid) {
-            System.out.println("Last Name Entered " + lastName + " is Valid.");
+            System.out.println("Mobile NUmber Entered " + mobileNum + " is Valid.");
         }
         else{
-            System.out.println("Entered Last Name is Invalid. Re-Enter Valid Last Name");
+            System.out.println("Mobile Number Entered is Invalid. Re-Enter Valid Mobile Number");
         }
     }
     public static void main(String[] args) {
-        System.out.println("Enter the Last Name: ");
+        System.out.println("Enter User Mobile Number: ");
         //Last Name First Character should be Capital and should have Minimum 3 Characters.
         //Calling the method to check Last Name Validation.
-        lastNameValidation();
+        mobileNumValidation();
     }
 }
