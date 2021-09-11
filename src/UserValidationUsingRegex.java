0dbe5321cf58af5fbcfad49d43f65ce709362c48
Sajan <sajanmhatre29@gmail.com>
2021-09-11 10:41:49 +0530

    Email Validation

--- src/UserValidationUsingRegex.java
+++ src/UserValidationUsingRegex.java
@@ -1,5 +1,41 @@
-public class UserValidationUsingRegex {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Validation...");
+// Java program to check if an email address
+// is valid using Regex.
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class UserValidationUsingRegex {
+    public static void emailValidation() {
+        //Take Input from the User
+        System.out.println("Enter your Email: ");
+        Scanner nw = new Scanner(System.in);
+        String email = nw.next();
+        //Pattern to Validate Email
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        //Conditions to validate Email
+        if (email == null) {
+//            return false;
+            System.out.println("No Email Entered.");
+        }
+       else if (pat.matcher(email).matches()){
+            System.out.println("Entered Email is Valid");
+        }
+        else{
+            System.out.println("Entered Email is Invalid");
+        }
+//        return pat.matcher(email).matches();
+    }
+
+    // driver class
+        public static void main(String[] args) {
+//        String email = "sajanmhatre@gmail.com";
+//      Calling Email Validation method
+        emailValidation();
+//        System.out.println(emailValidation());
     }
 }
