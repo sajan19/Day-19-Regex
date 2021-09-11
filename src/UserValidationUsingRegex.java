721a28fab5f733cbb81c7950424c4954e801c209
Sajan <sajanmhatre29@gmail.com>
2021-09-11 09:06:27 +0530

    Password Validation

--- src/UserValidationUsingRegex.java
+++ src/UserValidationUsingRegex.java
@@ -1,27 +1,60 @@
+
+// Java program to validate
+// the password using ReGex
+
 import java.util.Scanner;
-public class UserValidationUsingRegex {
-    //Instance Variable Declaration
-    private static String mobileNum;
-    //Method to check Mobile Number Validation
-    public static void mobileNumValidation(){
-        //Scanner object to take Input from the User
-        Scanner nw =new Scanner(System.in);
-        mobileNum = nw.next();
-//      boolean valid = name.matches("(?i)(^[a-z]+)[a-z .,-]((?! .,-)$){1,25}$");
-        //Pattern to check the Last Name
-        boolean valid = mobileNum.matches("(0|91)?[7-9][0-9]{9}");
-        //Condition to check Validity
-        if (valid) {
-            System.out.println("Mobile NUmber Entered " + mobileNum + " is Valid.");
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//import java.util.regex.*;
+class UserValidationUsingRegex {
+
+    // Method to validate the password.
+    public static boolean passwordValidation(){
+        //Take Input from the User
+        System.out.println("Enter Password: ");
+        Scanner ps = new Scanner(System.in);
+        String password = ps.next();
+        // Regex to check valid password.
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+
+        // If the password is empty
+        // return false
+        if (password == null) {
+            return false;
         }
-        else{
-            System.out.println("Mobile Number Entered is Invalid. Re-Enter Valid Mobile Number");
-        }
+
+        // Pattern class contains matcher() method
+        // to find matching between given password
+        // and regular expression.
+        Matcher m = p.matcher(password);
+
+        // Return if the password
+        // matched the ReGex
+        return m.matches();
     }
-    public static void main(String[] args) {
-        System.out.println("Enter User Mobile Number: ");
-        //Last Name First Character should be Capital and should have Minimum 3 Characters.
-        //Calling the method to check Last Name Validation.
-        mobileNumValidation();
+
+    // Driver Code.
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Password Validation...");
+//        passwordValidation();
+        //Print statement
+        System.out.println(passwordValidation());
+
+
+//        // Test Case 1:
+//        String str1 = "Geeks@portal20";
+//        System.out.println(passwordValidation(str1));
+
+
     }
 }
+
+
