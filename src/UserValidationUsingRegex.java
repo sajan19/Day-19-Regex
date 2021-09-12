c3dfe8b1d50480582a46331e2c784e4f5e7e1be5
Sajan <sajanmhatre29@gmail.com>
2021-09-12 19:27:50 +0530

    Sample Emails Validation

--- src/UserValidationUsingRegex.java
+++ src/UserValidationUsingRegex.java
@@ -1,41 +1,64 @@
-// Java program to check if an email address
-// is valid using Regex.
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class UserValidationUsingRegex {
-    public static void emailValidation() {
-        //Take Input from the User
-        System.out.println("Enter your Email: ");
-        Scanner nw = new Scanner(System.in);
-        String email = nw.next();
+//Java Program to validate Email Samples
+class Email{
+    //Declare ArrayList
+    ArrayList<String> emails;
+    //Constructor
+    public Email() {
+        this.emails = new ArrayList<>();
+    }
+    //Method to validate emails
+    void emailValidation(ArrayList emails) {
         //Pattern to Validate Email
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
                 "[a-zA-Z0-9_+&*-]+)*@" +
                 "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{2,7}$";
+                "A-Z]{2,17}$";
+        Pattern p = Pattern.compile(emailRegex);
+        for (Object e : emails) {
+            Matcher m = p.matcher((CharSequence) e);
+            System.out.println(e + " : " + m.matches());
 
-        Pattern pat = Pattern.compile(emailRegex);
-        //Conditions to validate Email
-        if (email == null) {
-//            return false;
-            System.out.println("No Email Entered.");
         }
-       else if (pat.matcher(email).matches()){
-            System.out.println("Entered Email is Valid");
-        }
-        else{
-            System.out.println("Entered Email is Invalid");
-        }
-//        return pat.matcher(email).matches();
-    }
-
-    // driver class
-        public static void main(String[] args) {
-//        String email = "sajanmhatre@gmail.com";
-//      Calling Email Validation method
-        emailValidation();
-//        System.out.println(emailValidation());
     }
 }
+class UserValidationUsingRegex {
+    public static void main(String[] args) {
+        System.out.println("Welocme to Validate Email Samples...");
+        Email obj = new Email();
+        //Valid Email Ids
+        obj.emails.add("abc@yahoo.com");
+        obj.emails.add("abc-100@yahoo.com");
+        obj.emails.add("abc.100@yahoo.com");
+        obj.emails.add("abc-100@abc.net");
+        obj.emails.add("abc.100@abc.com.au");
+        obj.emails.add("abc@1.com");
+        obj.emails.add("abc@gmail.com.com");
+        obj.emails.add("abc+100@gmail.com");
+        //Invalid Emails
+        obj.emails.add("abc");
+        obj.emails.add("abc@.com.my");
+        obj.emails.add("abc123@gmail.a");
+        obj.emails.add("abc123@.com");
+        obj.emails.add("abc123@.com.com");
+        obj.emails.add(".abc@abc.com");
+        obj.emails.add("abc()*@gmail.com");
+        obj.emails.add("abc@%*.com");
+        obj.emails.add("abc..2002@gmail.com");
+        obj.emails.add("abc.@gmail.com");
+        obj.emails.add("abc@abc@gmail.com");
+        obj.emails.add("abc@gmail.com.1a");
+//        obj.emails.add("abc@gmail.com.3a.au");
+//        obj.emails.add("");
+        //Method to print the Arraylist
+//        for (Object e: obj.emails) {
+//            System.out.println(e);
+//        }
+        //Calling the Method to Check Email validity
+        obj.emailValidation(obj.emails);
+    }
+}
+
