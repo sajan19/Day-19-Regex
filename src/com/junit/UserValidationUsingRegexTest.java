d37aad9f4c51be33cd1977dbdd6e6d1ee969289c
Sajan <sajanmhatre29@gmail.com>
2021-09-14 13:20:47 +0530

    Parameterised Test

--- src/com/junit/UserValidationUsingRegexTest.java
+++ src/com/junit/UserValidationUsingRegexTest.java
@@ -1,84 +1,32 @@
-package com.junit;
-
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
+public class EmailValidation{
+    String email;
+    boolean expectedResult;
 
-public class UserValidationUsingRegexTest {
-    //First Name Test Cases
-    @Test
-    public void givenFirstName_whenProper_ShouldReturnTrue(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.firstNameValidaion("Sajan");
-//        Assert.assertEquals(true,result);
-        Assert.assertTrue(result);
+    public EmailValidation(String email, boolean expectedResult) {
+        this.email = email;
+        this.expectedResult = expectedResult;
     }
-    @Test
-    public void givenFirstName_whenNotProper_ShouldReturnFalse(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.firstNameValidaion("Sa");
-        Assert.assertFalse(result);
-    }
-    //Last Name Test Cases
-  @Test
-    public void givenLastName_whenProper_ShouldReturnTrue(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.lastNameValidaion("Mhatre");
-//        Assert.assertEquals(true,result);
-        Assert.assertTrue(result);
-    }
-    @Test
-    public void givenLastName_whenNotProper_ShouldReturnFalse(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.lastNameValidaion("Mh");
-//        Assert.assertEquals(false,result);
-        Assert.assertFalse(result);
-    }
+    @Parameterized.Parameters
+    public static Collection data(){
+        return Arrays.asList(new Object[][]{ {"abc@yahoo.com", true},
+            {"abc-100@yahoo.com", true}, {"abc.100@yahoo.com",true},{"abc-100@abc.net",true},
+            {"abc.100@abc.com.au",true},{"abc@1.com",true},{"abc@gmail.com.com",true},{"abc+100@gmail.com",true},
+            {"abc",true},{"abc@.com.my",true},{"bc123@gmail.a",true},{"abc123@.com",true},
+            {"abc123@.com.com",true},{".abc@abc.com",true},{"abc()*@gmail.com",true},{"abc@%*.com",true},{"\"abc..2002@gmail.com",true},
+            {"abc.@gmail.com",true},{"abc@abc@gmail.com",true},{"abc@gmail.com.1a",true}
 
-    //Email Test Cases
-    @Test
-    public void givenEmail_whenProper_ShouldReturnTrue(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.emailValidation("sajanmhatre@gmail.com");
-//        Assert.assertEquals(true,result);
-        Assert.assertTrue(result);
+        });
     }
     @Test
-    public void givenEmail_whenNotProper_ShouldReturnFalse(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.emailValidation("abc@gmail.com.1a");
-//        Assert.assertEquals(false,result);
-        Assert.assertFalse(result);
-    }
-    //Mobile Number Cases
-    @Test
-    public void givenMobileNumber_whenProper_ShouldReturnTrue(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.mobileNumValidation("91 9930456789");
-//        Assert.assertEquals(true,result);
-        Assert.assertTrue(result);
-    }
-    @Test
-    public void givenMobileNumber_whenNotProper_ShouldReturnFalse(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.mobileNumValidation("0 4856432999");
-//        Assert.assertEquals(true,result);
-        Assert.assertFalse(result);
-    }
-    //Password Test Cases
-    @Test
-    public void givenPassword_whenProper_ShouldReturnTrue(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.passwordValidation("Sajan@4564");
-//        Assert.assertEquals(true,result);
-        Assert.assertTrue(result);
-    }
-    @Test
-    public void givenPassword_whenNotProper_ShouldReturnFalse(){
-        UserValidationUsingRegex validation = new UserValidationUsingRegex();
-        boolean result = validation.passwordValidation("77@4564");
-//        Assert.assertEquals(true,result);
-        Assert.assertFalse(result);
+    void givenEmail_ShouldReturnAsPerThePArameterisedResult(){
+
     }
 }
